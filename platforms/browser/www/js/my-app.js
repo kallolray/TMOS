bf2a8448ff94039c09104e3013c866cebd3c37d5
Kallol Ray <rayk@timken.com>
2019-04-02 19:05:49 +0530

    test

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -8,6 +8,9 @@ var app = new Framework7({
     id: 'com.app.test',
 });
 
+var host = 'http://localhost:62029';
+//var host = 'http://tilhdev02/tmosdata';
+var mcList = {};
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
@@ -21,23 +24,56 @@ var mainView = app.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     $$('#msg').text('Device Ready, making Ajax Request');
+    getMCList();
+});
+
+function getMCList(){
     $$.ajax({
-        url: 'http://tilhdev02/tmosdata/api/hello/ray/kallol',
+        url: host + '/api/plc/mclist',
         cache: false,
         dataType: 'json',
         type: 'GET',
-        success: function(r_data){
-            $$('#msg').text(r_data);
-            app.alert("Got data back");
-            },
+        crossOrigin: true,
+        success: function(data){
+            var tbl = "";
+            for(let i=0; i < data.length; ++i){
+                if(i%2 == 0) 
+                    tbl += '<tr style="height:30%">';
+                tbl += `<td id='mc${i}' style="width:50%">${data[i]}<br><span id='dtl${i}'></span></td>`;
+                if(i%2 == 1)
+                    tbl +='</tr>';
+                mcList[data[i]] = i;
+            }
+            $$('#mcList').append($$(tbl));
+            updateAndon();
+        },
         error: function(error){
             app.alert("Error");
           $$('#msg').text("Error : " + error);
         }
-  });    
-});
+    });    
+}
 
-
+function updateAndon(){
+    $$.ajax({
+        url: host + '/api/plc/activeandon',
+        cache: false,
+        dataType: 'json',
+        type: 'GET',
+        crossOrigin: true,
+        success: function(data){
+            for(let i=0; i < data.length; ++i){
+                var x = mcList[data[i].LINE];
+                $$('#mc'+x).css('background-color', 'red');
+                $$('#dtl'+x).text(`${data[i].MC},${data[i].TIME}`);
+            }
+        },
+        error: function(error){
+            app.alert("Error");
+          $$('#msg').text("Error : " + error);
+        }
+    });    
+}
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
